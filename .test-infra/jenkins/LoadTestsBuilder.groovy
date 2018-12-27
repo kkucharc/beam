45039ba99e4d65f23a2327fa7b6ae4e4a3bb4fa8
Kasia <katarzyna.kucharczyk@polidea.com>
2018-12-27 15:12:55 +0100

    Check

--- .test-infra/jenkins/LoadTestsBuilder.groovy
+++ .test-infra/jenkins/LoadTestsBuilder.groovy
@@ -17,6 +17,7 @@
  */
 
 import CommonJobProperties as commonJobProperties
+import CommonTestProperties as commonTestProperties
 
 // Class for building Load Tests jobs and suites
 class LoadTestsBuilder {
@@ -26,29 +27,14 @@ class LoadTestsBuilder {
             tempLocation        : 'gs://temp-storage-for-perf-tests/loadtests',
     ]
 
-    enum Runner {
-        DATAFLOW("DataflowRunner", ":beam-runners-google-cloud-dataflow-java"),
-        SPARK("SparkRunner", ":beam-runners-spark"),
-        FLINK("FlinkRunner", ":beam-runners-flink_2.11"),
-        DIRECT("DirectRunner", ":beam-runners-direct-java")
-
-        private final String option
-        private final String dependency
-
-        Runner(String option, String dependency) {
-            this.option = option
-            this.dependency = dependency
-        }
-    }
-
-    static void buildTest(context, String title, Runner runner, Map<String, Object> jobSpecificOptions, String mainClass) {
+    static void buildTest(context, String title, commonTestProperties.Runner runner, Map<String, Object> jobSpecificOptions, String mainClass) {
         Map<String, Object> options = jobSpecificOptions + defaultOptions
         options.put('runner', runner.option)
 
         suite(context, title, runner, options, mainClass)
     }
 
-    static void suite(context, String title, Runner runner, Map<String, Object> options, String mainClass) {
+    static void suite(context, String title, commonTestProperties.Runner runner, Map<String, Object> options, String mainClass) {
         context.steps {
             shell("echo *** ${title} ***")
             gradle {
