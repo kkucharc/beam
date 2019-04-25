41a0dbaeb6211659487b7a2b74a7a83114b67c89
Kasia Kucharczyk <katarzyna.kucharczyk@polidea.com>
2019-04-25 13:43:40 +0200

    Moved suites to one file

--- .test-infra/jenkins/job_LoadTests_Python.groovy
+++ .test-infra/jenkins/job_LoadTests_Python.groovy
@@ -146,3 +146,42 @@ PhraseTriggeringPostCommitBuilder.postCommitJob(
 CronJobBuilder.cronJob('beam_LoadTests_Python_GBK_Dataflow_Batch', 'H 12 * * *', this) {
         loadTestsBuilder.loadTests(delegate, CommonTestProperties.SDK.PYTHON, loadTestConfigurations, CommonTestProperties.TriggeringContext.POST_COMMIT, "GBK", "batch")
 }
+
+
+def loadTestWithReiterateConfigurations = [
+        [
+                title        : 'GroupByKey Python Load test: reiterate 4 times 10kB values',
+                itClass      : 'apache_beam.testing.load_tests.group_by_key_test:GroupByKeyTest.testGroupByKey',
+                runner       : CommonTestProperties.Runner.DATAFLOW,
+                sdk          : CommonTestProperties.SDK.PYTHON,
+                jobProperties: [
+                        job_name             : 'load-tests-python-dataflow-batch-gbk-6-' + now,
+                        project              : 'apache-beam-testing',
+                        temp_location        : 'gs://temp-storage-for-perf-tests/loadtests',
+                        publish_to_big_query : true,
+                        metrics_dataset      : 'load_test',
+                        metrics_table        : 'python_dataflow_batch_gbk_6',
+                        input_options        : '\'{"num_records": 20000000,' +
+                                '"key_size": 10,' +
+                                '"value_size": 90}\'',
+                        iterations           : 4,
+                        fanout               : 1,
+                        max_num_workers      : 5,
+                        num_workers          : 5,
+                        autoscaling_algorithm: "NONE"
+                ]
+        ]
+]
+
+PhraseTriggeringPostCommitBuilder.postCommitJob(
+        'beam_Python_LoadTests_GBK_Dataflow_Batch_With_Reiteration',
+        'Run Python Load Tests GBK Dataflow Batch With Reiteration',
+        'Load Tests Python GBK Dataflow Batch suite with reiteration',
+        this
+) {
+        loadTestsBuilder.loadTests(delegate, CommonTestProperties.SDK.PYTHON, loadTestWithReiterateConfigurations, CommonTestProperties.TriggeringContext.PR, "GBK", "batch")
+}
+
+CronJobBuilder.cronJob('beam_LoadTests_Python_GBK_Dataflow_With_Reiteration_Batch', 'H 12 * * *', this) {
+        loadTestsBuilder.loadTests(delegate, CommonTestProperties.SDK.PYTHON, loadTestWithReiterateConfigurations, CommonTestProperties.TriggeringContext.POST_COMMIT, "GBK", "batch")
+}
